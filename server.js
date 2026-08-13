c5ab9d91cb704c3744b63c7484cfed0e764de023
Jose Manuel Santos <msantos@192.168.1.5>
2026-08-12 18:15:18 -0600

    Cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {errorHandler} = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 const connectDB = require('./connect/database');
+const Cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +11,7 @@ connectDB();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+app.use(Cors());
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
